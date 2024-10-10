cdb928b539556ef7c815f793e5358e17c799b6be
Goddy1771 <kiptumgodwin0@gmail.com>
2024-10-10 20:57:08 +0300

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main() {
+    
+    int n, pos, arr[100], i, num;
+    
+    printf(" Enter the size of an array (Max 100) \n");
+    scanf( " %d ", &n);
+    
+    printf( " Enter the element of an array \n");
+    
+    for(i = 0; i < n; i++) {
+        scanf(" %d ", &arr[i]);
+    }
+    
+    " Enter the position \n");
+    scanf(" %d ", &pos);
+    
+    printf(" Enter number to be inserted \n");
+    scanf(" %d ", &num);
+    
+    for( i = n; i >= pos; i--) {
+        arr[i] = arr[i-1];
+    }
+    
+    arr[pos-1] = num;
+    
+    n++;
+    
+    
+    printf(" Array after inserting new element \n");
+    
+    for( i = 0; i < n; i++) {
+        printf(" %d ", arr[i]);
+    }
+    return 0;
+}
